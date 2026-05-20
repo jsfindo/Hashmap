bc917b0ea33fa02cb24c8c49e7b31af8c657cc79
jsfindo <johannes.sivola@gmail.com>
2026-05-20 20:15:22 +0300

    slsl

---
+++ index.js
@@ -0,0 +1,24 @@
+class Hashmap{
+    constructor(initialCapacity = 16, loadFactor = 0.75) {
+    this.capacity = initialCapacity;
+    this.loadFactor = loadFactor;
+
+    }
+
+     hash(key) {
+  let hashCode = 0;
+
+  const primeNumber = 31;
+  for (let i = 0; i < key.length; i++) {
+    hashCode = primeNumber * hashCode + key.charCodeAt(i);
+  }
+
+  return hashCode;
+} 
+
+set(key, value){
+    
+}
+
+
+}
